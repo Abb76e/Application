bc2b86dfec62b27365041f11a04842d6406c3377
Abbas <abbasemad@gmail.com>
2021-12-10 18:12:06 +0300

    committed

---
+++ music/js/music-list.js
@@ -0,0 +1,128 @@
+let allMusic = [
+    {
+      name: "CLOUDS",
+      artist: "NF",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Let You Down",
+      artist: "NF",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "When I Grow Up",
+      artist: "NF",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "The Search",
+      artist: "NF",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "If You Want Love",
+      artist: "NF",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: " Story",
+      artist: "NF",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+        name: "Lost",
+        artist: "NF",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Trust",
+        artist: "NF",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Just Like You",
+        artist: "NF",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Paralyzed",
+        artist: "NF",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Lie",
+        artist: "NF",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Leave Me Alone",
+        artist: "NF",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Drifting ",
+        artist: "NF",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "That's A Joke",
+        artist: "NF",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: " Layers",
+        artist: "NF",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Prideful",
+        artist: "NF",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Time",
+        artist: "NF",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "My Stress",
+        artist: "NF",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+      name: "My Stress",
+      artist: "NF",
+      img: "music-19",
+      src: "music-19"
+    },
+    {
+      name: "Paid My Dues",
+      artist: "NF",
+      img: "music-20",
+      src: "music-20"
+    },
+    {
+      name: "Oh Lord",
+      artist: "NF",
+      img: "music-21",
+      src: "music-21"
+    },
+  ];
